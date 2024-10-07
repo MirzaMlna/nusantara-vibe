9d370192a6f906b0ee3a412a6e804ea4b3561661
MirzaMlna <mirzamaulana713@gmail.com>
2024-10-07 23:57:14 +0800

    💄 Make Over Home Page (Tryin to use @include)

---
+++ resources/views/home/fabric_product/index.blade.php
@@ -0,0 +1,13 @@
+<h3 class="text-app fw-bold">Kain Batik </h3>
+<div class="app-line"></div>
+<div class="card mt-3" style="width: 18rem;">
+    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Product Image" />
+    <div class="card-body">
+        <h5 class="card-title fw-light text-truncate">Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Quidem, nam?</h5>
+        <p class="card-text fw-bold">Rp100.000 <span class="fw-light">/ meter</span></p>
+        <a href="#" class="btn btn-success">Beli</a>
+        <a href="#" class="btn btn-secondary">Detail</a>
+        <a href="#" class="btn btn-dark"><i class="bi bi-cart-plus-fill"></i></a>
+    </div>
+</div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,29 +10,23 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-app" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-lg bg-app sticky-top" data-bs-theme="dark">
         <div class="container">
             <a class="navbar-brand mx-auto fw-bold">Nusatara <span
                     class="bg-light text-app px-2 rounded-1">Vibe</span></a>
         </div>
     </nav>
     <main>
-        <section class="container mt-5">
-            <h3 class="text-app fw-bold">Produk Teratas <i class="bi bi-bag-check-fill"></i></h3>
-            <div class="app-line"></div>
-            <div class="card my-3" style="width: 18rem;">
-                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Product Image" />
-                <div class="card-body">
-                    <h5 class="card-title fw-light text-truncate">Batik Anjay Banar Banar Banar</h5>
-                    <p class="card-text fw-bold">Rp100.000</p>
-                    <a href="#" class="btn btn-success">Beli</a>
-                    <a href="#" class="btn btn-secondary">Selengkapnya</a>
-                </div>
-            </div>
+        <section class="container my-5">
+            @include('home.new_product.index')
+        </section>
+        <section class="container my-5">
+            @include('home.fabric_product.index')
+
         </section>
     </main>
 
-
+    @include('components.footer')
 </body>
 
 </html>
